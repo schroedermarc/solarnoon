9373c124c4978deda5454d2a0a4b21a12b67882c
Marc Schroeder <marcschroeder44@gmail.com>
2018-10-13 14:36:41 -0700

    api data working

--- index.js
+++ index.js
@@ -1,38 +1,43 @@
 const express = require('express');
 const request = require('request');
-const app     = express();
-const PORT    = 3000;
+const app = express();
+const PORT = 3000;
 
 // tell our app where to serve our static files
 app.use(express.static('public'));
 
 // --------------------------------------------------------
 // define a route - what happens when people visit /
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-// --------------------------------------------------------
-// wrap an api request in our own endpoint
-app.get('/getFilmData', function(req, res) {
-  
-  let ghibliFilmsUrl = 'https://ghibliapi.herokuapp.com/films';
-  let options = {
-    json: true 
-  };
+app.get('/getSunData', (req, res) => {
+  let url = "https://api.sunrise-sunset.org/json"
+  // url = "https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400"
+  //https://api.sunrise-sunset.org/json?lat=34.02&lng-110.7
+  //lat=34.02&lng=-110.7
+  url = "https://api.sunrise-sunset.org/json?lat=" + req.query.lat + "&lng=" + req.query.lng + "&formatted=0";
 
-  // make an api request to the ghibli api /films endpoint
-  request(ghibliFilmsUrl, options, function(error, response, body) {
+  console.log(url);
+  console.log(req.query.lat, req.query.lng);
+
+  //request.query.token
+
+
+  request(url, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       res.send(body);
     } else {
       res.send(err);
     }
-  }); 
+  });
+
 });
+
 
 // --------------------------------------------------------
 // tell our app where to listen for connections
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log('listening on PORT: ' + PORT);
 });
--- public/js/sketch.js
+++ public/js/sketch.js
@@ -1,14 +1,105 @@
-let button;
-let filmsData = [];
+let worldMapImg;
+let mapMarkerDetails = {
+  x: 221,
+  y: 50,
+  color: "(0,0,0,255)"
+}
+
+let selectedCoords = {
+  lat: 0,
+  long: 0
+}
+
+
+mapCoords = {
+  maxX: 0,
+  minX: 0,
+  maxY: 0,
+  minY: 0
+}
+
+let sunrise;
+let sunset;
+
+function preload() {
+  worldMapImg = loadImage("/assets/worldMap.jpg");
+
+}
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
 
+  imageMode(CENTER);
+
+  mapCoords.minX = width / 2 - 200;
+  mapCoords.maxX = width / 2 + 200;
+  mapCoords.minY = 50;
+  mapCoords.maxY = 350;
 
 }
 
 
 function draw() {
   background(255);
+  image(worldMapImg, width / 2, 200, 400, 300);
 
+  fill(mapMarkerDetails.color);
+  ellipse(mapMarkerDetails.x, mapMarkerDetails.y, 10, 10);
+
+}
+
+function mouseClicked() {
+  // x: 221 - 620
+  // y: 50 - 349
+
+
+  // console.log('');
+
+  if (mouseX >= mapCoords.minX && mouseX <= mapCoords.maxX && mouseY >= mapCoords.minY && mouseY <= mapCoords.maxY) {
+
+    // y = latitude
+    // x = longitude
+
+
+
+    mapMarkerDetails.x = mouseX;
+    mapMarkerDetails.y = mouseY;
+
+    // console.log(mouseX, mouseY);
+
+    selectedCoords.lat = (map(mouseY, mapCoords.minY, mapCoords.maxY, 83, -75));
+    selectedCoords.long = (map(mouseX, mapCoords.minX, mapCoords.maxX, -180, 180));
+
+    getSunData();
+
+  }
+
+}
+
+let params = {
+  lat: 0,
+  long: 0
+}
+
+function getSunData() {
+
+  let url = "/getSunData?lat=" + selectedCoords.lat + "&lng=" + selectedCoords.long;
+  console.log(url);
+
+
+  httpGet(url, (response) => {
+
+    let sunData = JSON.parse(response);
+
+    // console.log('getting sun data', sunData.results);
+
+
+    sunset = sunData.results.sunset;
+    sunrise = sunData.results.sunrise;
+
+
+    console.log(sunset);
+    console.log(sunrise);
+
+  });
 }
